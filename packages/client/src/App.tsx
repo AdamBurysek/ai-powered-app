e23c84fba920ec99282cdbc64e7632b03add698d
Adam Buryšek <114564710+AdamBurysek@users.noreply.github.com>
2025-08-28 13:39:43 +0200

    fead: add husky

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,22 +1,22 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import { Button } from './components/ui/button';
 
 function App() {
    const [message, setMessage] = useState('');
 
-   useEffect(() => {
-      fetch('/api/hello')
-         .then((response) => response.json())
-         .then((data) => {
-            setMessage(data.message);
-         });
-   }, []);
+   const fetchData = async () => {
+      const response = await fetch('/api/hello');
+      const data = await response.json();
+
+      setMessage(data.message);
+   };
 
    return (
       <div className="flex flex-col items-center justify-center h-screen gap-4">
-         <h1 className="text-3xl font-bold">{message}</h1>
-         <Button>Click me</Button>
+         {message && <h1 className="text-3xl font-bold">{message}</h1>}
+
+         <Button onClick={fetchData}>Fetch Data</Button>
       </div>
    );
 }
